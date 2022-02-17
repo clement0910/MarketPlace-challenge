223759f1d20e471f017d3bf3f7c620cda8fe7d75
Clément Sapt <csapt2001@gmail.com>
2022-02-17 06:03:17 +0100

    change tailwind config for daisyui :wrench:

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    content: [
+  content: [
       "./**/*.html.erb",
       "./**/**/*.html.erb",
       "./**/*.html+phone.erb",
       "./app/helpers/**/*.rb",
       "./app/javascript/**/*.js",
-    ],
+  ],
   plugins: [
     require('@tailwindcss/typography'),
     require('daisyui')
   ],
   daisyui: {
     styled: true,
-    themes: true,
+    themes: false,
     base: true,
     utils: true,
     logs: true,
