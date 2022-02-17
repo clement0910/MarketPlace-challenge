501c6b948833a07d49a537b45e1e1e9bf5f325ab
Clément Sapt <csapt2001@gmail.com>
2022-02-17 05:23:10 +0100

    launch project with webpacker, tailwindcss and daisyui :rocket:

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: [
+      "./**/*.html.erb",
+      "./**/**/*.html.erb",
+      "./**/*.html+phone.erb",
+      "./app/helpers/**/*.rb",
+      "./app/javascript/**/*.js",
+    ],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui')
+  ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+}
